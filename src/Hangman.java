bba60801f9a9aa39cc4ff5b49786f239ee114791
DaxGute <league@Mac-mini-4.local>
2018-09-05 18:27:20 -0700

    9_5_18
    Almost done with Hangman. Next step is to work on the KeyListener!!!!

--- src/Hangman.java
+++ src/Hangman.java
@@ -5,16 +5,29 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Hangman {
+	int lives;
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JLabel guessLabel = new JLabel();
+	JLabel livesLabel = new JLabel();
+	JLabel solvedLabel = new JLabel();
+	JLabel wordLabel = new JLabel();
+	static Stack<String> wordStack = new Stack<>(); //Static can 
+
 	public static void main(String[] args) {
 		String numOfWordsString = JOptionPane
 				.showInputDialog("Welcome to Hangman! How many rounds would you like to play?");
 		int numOfWordsInt = Integer.parseInt(numOfWordsString); // Changes the number string into an number int
 		ArrayList<String> wordList = new ArrayList<>(numOfWordsInt);
-		Stack<String> wordStack = new Stack<>();
 
+		// This will read each word from the "instructions.txt" and adds the words into
+		// "wordList"
 		try {
 			Scanner sc = new Scanner(new File("src/dictionary.txt"));
 			for (int i = 0; i < numOfWordsInt; i++) {
@@ -23,17 +36,51 @@ public class Hangman {
 			}
 			sc.close();
 
+			// For errors
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		// The words will be in a random order
 		Random ran = new Random();
-		
-		while(wordList.size()>0) {
+
+		while (wordList.size() > 0) {
 			int ranNum = ran.nextInt(wordList.size());
 			wordStack.push(wordList.remove(ranNum));
 			System.out.println(ranNum);
+
 		}
+
+		Hangman hangman = new Hangman();
+	}
+
+
+
+	// Constructor for my UI
+	Hangman() {
+
+		f.add(p);
+		p.add(guessLabel);
+		p.add(livesLabel);
+		p.add(solvedLabel);
+		p.add(wordLabel);
+
+		f.pack();
+		f.setVisible(true);
+
+		guessLabel.setText("Guess a letter.");
+		livesLabel.setText("You have ");
+		wordLabel.setText("You have solved ");
+
+		String pop = wordStack.pop();
+		int popChar = 4;
+		String blankLine = "";
+
+		for (int j = 0; j < popChar; j++) {
+			blankLine += "_ ";
+		}
+		wordLabel.setText("" + blankLine);
+		System.out.println(blankLine);
 	}
 }
